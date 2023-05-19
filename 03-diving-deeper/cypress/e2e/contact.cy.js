f8d66d6d9974c06c70b25393b4308de1cb889e67
Nebura <hamklieneduprofesional@gmail.com>
2023-05-18 19:31:04 -0600

    Cypress stubs, spies, fixtures and manipulation clock

--- 03-diving-deeper/cypress/e2e/contact.cy.js
+++ 03-diving-deeper/cypress/e2e/contact.cy.js
@@ -1,10 +1,15 @@
 /// <reference types="Cypress"/>
 
 describe('contact form', () => {
+  beforeEach(() => {
+    cy.visit('/about')
+  })
+
   it('should submit the form', () => {
-    cy.visit('http://localhost:5173/about')
-    cy.get('[data-cy="contact-input-message"]').type('Some message')
-    cy.get('[data-cy="contact-input-name"]').type('Maria Juana')
+    // cy.get('[data-cy="contact-input-message"]').type('Some message')
+    // cy.get('[data-cy="contact-input-name"]').type('Maria Juana')
+    cy.getById('contact-input-message').type('Some message')
+    cy.getById('contact-input-name').type('Maria Juana')
     // cy.get('[data-cy="contact-input-email"]').type('example@example.com')
     // cy.get('[data-cy="contact-btn-submit"]')
     //   .contains('Send Message')
@@ -16,15 +21,17 @@ describe('contact form', () => {
       //   expect(element.text()).to.eq('Send Message')
     }) //Esto me retorna en el then un o unos elementos, dependiendo que estoy apuntando
     cy.screenshot() //Manera de tomar capturas de pantalla manualmente
-    cy.get('[data-cy="contact-input-email"]').type('example@example.com{enter}') //Una tecla especial es el {enter}
+    // cy.get('[data-cy="contact-input-email"]').type('example@example.com') //Una tecla especial es el {enter}
+    cy.getById('contact-input-email').type('example@example.com') //Custom query
+
     cy.get('[data-cy="contact-btn-submit"]').as('submitBtn') //De esta manera se crea un alias para retutilizarlo
-    cy.get('@submitBtn').click()
+    cy.submitForm()
+    // cy.get('@submitBtn').click()
     cy.get('@submitBtn').contains('Sending...')
     cy.get('@submitBtn').should('have.attr', 'disabled')
   })
 
   it('should validate the form input', () => {
-    cy.visit('http://localhost:5173/about')
     cy.get('[data-cy="contact-btn-submit"]').click()
     // cy.get('[data-cy="contact-btn-submit"]').contains('Send message') //Esto no he una buena practica
     cy.get('[data-cy="contact-btn-submit"]').then((element) => {
